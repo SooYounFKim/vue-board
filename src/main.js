f3182f31baf8427ff7b7a3afbbc4762e42b31fde
SooYounFKim <sooyoun.f.kim@gmail.com>
2021-11-15 18:25:20 +0900

    axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routes';
+import axios from 'axios';
 
 Vue.config.productionTip = true;
+
+Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
